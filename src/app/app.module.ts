48594fd99bba62f84733a370b670d117896b2ac0
Rodrigo de Almeida Muniz <r.de.almeida.muniz@avanade.com>
2020-12-31 11:37:49 -0300

    atualizações

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 import { ProducListComponent } from './products/product-list.component';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ConvertToSpacePipe } from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProducListComponent
+    ProducListComponent,
+    ConvertToSpacePipe,
+    StarComponent
   ],
   imports: [
     BrowserModule,
